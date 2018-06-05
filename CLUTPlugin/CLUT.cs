5bbc597bfc24d8de3433e91048542d6ef8f6938b
Zackin5 <zachin55@gmail.com>
2018-06-04 21:51:57 -0500

    Initial implementation of Steenberg's C code Updated README.md

--- CLUTPlugin/CLUT.cs
+++ CLUTPlugin/CLUT.cs
@@ -126,7 +126,7 @@ namespace CLUT
             props.Add(new StringProperty(PropertyNames.DirSetting, "", 255));
             Amount2Options Amount2Default = (Enum.IsDefined(typeof(Amount2Options), 0)) ? (Amount2Options)0 : 0;
             props.Add(StaticListChoiceProperty.CreateForEnum<Amount2Options>(PropertyNames.DirFiles, Amount2Default, false));
-            props.Add(new BooleanProperty(PropertyNames.checkk, true));
+            props.Add(new BooleanProperty(PropertyNames.checkk, false));
 
             return new PropertyCollection(props);
         }
@@ -181,7 +181,143 @@ namespace CLUT
 
         // Source: http://www.quelsolaar.com/technology/clut.html
 
-        ColorBgra correctPixel(ColorBgra pixelColour, Bitmap clut, int level)
+        class ClutData
+        {
+            public float[] data;
+            public int x;
+            public int y;
+
+            public ClutData(string filepath)
+            {
+                Bitmap clutBitmap = new Bitmap(Image.FromFile(filepath));
+                
+                if(clutBitmap.Width != clutBitmap.Height)
+                {
+                    data = null;
+                    return;
+                }
+
+                x = clutBitmap.Width;
+                y = clutBitmap.Height;
+                
+                data = new float[x * y * 3];
+
+                // 32bit
+                for( int j = 0; j < y; j++)
+                {
+                    for(int i = 0; i < x; i++)
+                    {
+                        data[((y - j - 1) * x + i) * 3 + 2] = clutBitmap.GetPixel(i, j).R / 255.0f;
+                        data[((y - j - 1) * x + i) * 3 + 1] = clutBitmap.GetPixel(i, j).G / 255.0f;
+                        data[((y - j - 1) * x + i) * 3 + 0] = clutBitmap.GetPixel(i, j).B / 255.0f;
+                    }
+                }
+            }
+
+            public Bitmap Bitmap()
+            {
+                Bitmap returnBitmap = new Bitmap(x, y);
+
+                for (int j = 0; j < y; j++)
+                {
+                    for (int i = 0; i < x; i++)
+                    {
+                        float r, g, b;
+
+                        r = data[((y - j - 1) * x + i) * 3 + 2] * 255.0f;
+                        g = data[((y - j - 1) * x + i) * 3 + 1] * 255.0f;
+                        b = data[((y - j - 1) * x + i) * 3 + 0] * 255.0f;
+
+                        returnBitmap.SetPixel(i, j, ColorBgra.FromBgr((byte)b, (byte)g, (byte)r));
+                    }
+                }
+
+                return returnBitmap;
+            }
+        }
+
+        ColorBgra correctPixel(ColorBgra pixelColor, ClutData clut, int level)
+        {
+            int color, red, green, blue, i, j;
+            float r, g, b;
+            float[] tmp = new float[6];
+            float[] output = new float[3];
+            float[] input = new float[]{
+                pixelColor.R / 255.0f,
+                pixelColor.G / 255.0f,
+                pixelColor.B / 255.0f,
+            };
+            
+            level *= level;
+                        
+            red = (int)(input[0] * (level - 1));
+            if (red > level - 2)
+                red = level - 2;
+            if (red < 0)
+                red = 0;
+
+            green = (int)(input[1] * (level - 1));
+            if (green > level - 2)
+                green = level - 2;
+            if (green < 0)
+                green = 0;
+
+            blue = (int)(input[2] * (level - 1));
+            if (blue > level - 2)
+                blue = level - 2;
+            if (blue < 0)
+                blue = 0;
+
+            r = input[0] * (level - 1) - red;
+            g = input[1] * (level - 1) - green;
+            b = input[2] * (level - 1) - blue;
+
+            color = red + green * level + blue * level * level;
+
+            i = color * 3;
+            j = (color + 1) * 3;
+
+            tmp[0] = clut.data[i++] * (1 - r) + clut.data[j++] * r;
+            tmp[1] = clut.data[i++] * (1 - r) + clut.data[j++] * r;
+            tmp[2] = clut.data[i] * (1 - r) + clut.data[j] * r;
+
+            i = (color + level) * 3;
+            j = (color + level + 1) * 3;
+
+            tmp[3] = clut.data[i++] * (1 - r) + clut.data[j++] * r;
+            tmp[4] = clut.data[i++] * (1 - r) + clut.data[j++] * r;
+            tmp[5] = clut.data[i] * (1 - r) + clut.data[j] * r;
+
+            output[0] = tmp[0] * (1 - g) + tmp[3] * g;
+            output[1] = tmp[1] * (1 - g) + tmp[4] * g;
+            output[2] = tmp[2] * (1 - g) + tmp[5] * g;
+
+            i = (color + level * level) * 3;
+            j = (color + level * level + 1) * 3;
+
+            tmp[0] = clut.data[i++] * (1 - r) + clut.data[j++] * r;
+            tmp[1] = clut.data[i++] * (1 - r) + clut.data[j++] * r;
+            tmp[2] = clut.data[i] * (1 - r) + clut.data[j] * r;
+
+            i = (color + level + level * level) * 3;
+            j = (color + level + level * level + 1) * 3;
+
+            tmp[3] = clut.data[i++] * (1 - r) + clut.data[j++] * r;
+            tmp[4] = clut.data[i++] * (1 - r) + clut.data[j++] * r;
+            tmp[5] = clut.data[i] * (1 - r) + clut.data[j] * r;
+
+            tmp[0] = tmp[0] * (1 - g) + tmp[3] * g;
+            tmp[1] = tmp[1] * (1 - g) + tmp[4] * g;
+            tmp[2] = tmp[2] * (1 - g) + tmp[5] * g;
+
+            output[0] = output[0] * (1 - b) + tmp[0] * b;
+            output[1] = output[1] * (1 - b) + tmp[1] * b;
+            output[2] = output[2] * (1 - b) + tmp[2] * b;
+            
+            return ColorBgra.FromBgra((byte)(output[2] * 255.0f), (byte)(output[1] * 255.0f), (byte)(output[0] * 255.0f), pixelColor.A);
+        }
+
+        ColorBgra correctPixelCheap(ColorBgra pixelColour, Bitmap clut, int level)
         {
             int cubeSize = level * level;
             byte alpha = pixelColour.A;
@@ -216,14 +352,19 @@ namespace CLUT
             int level;
             
             // Get CLUT image
-            Bitmap clutBitmap = new Bitmap(Image.FromFile("D:\\zachi\\Documents\\My Game DLLs\\Graphic Injectors\\Shaders\\HaldCLUT\\CLUTgallery\\Identity_level_8.HCLUT.png"));
-
+            //Bitmap clutBitmap = new Bitmap(Image.FromFile("D:\\zachi\\Documents\\My Game DLLs\\Graphic Injectors\\Shaders\\HaldCLUT\\CLUTgallery\\Identity_level_8.HCLUT.png"));
+            ClutData clut = new ClutData("D:\\zachi\\Documents\\My Game DLLs\\Graphic Injectors\\Shaders\\HaldCLUT\\CLUTgallery\\Identity_level_8.HCLUT.png");
+            
             // Calculate image's cubic root
-            for (level = 1; level * level * level < clutBitmap.Height; level++) ;
+            for (level = 1; level * level * level < clut.x; level++);
+
+            if (level * level * level > clut.x)
+                return;
 
             for (int y = rect.Top; y < rect.Bottom; y++)
             {
                 if (IsCancelRequested) return;
+
                 for (int x = rect.Left; x < rect.Right; x++)
                 {
                     currentPixel = src[x, y];
@@ -232,12 +373,12 @@ namespace CLUT
 
                     if (Checkk)
                     {
-                        var nucl = correctPixel(currentPixel, clutBitmap, level);
+                        var nucl = correctPixel(currentPixel, clut, level);
 
                         currentPixel = ColorBgra.FromBgra((byte)(nucl.B - prevPix.B), (byte)(nucl.G - prevPix.G), (byte)(nucl.R - prevPix.R), currentPixel.A);
                     }
                     else
-                        currentPixel = correctPixel(currentPixel, clutBitmap, level);
+                        currentPixel = correctPixel(currentPixel, clut, level);
 
                     // Render to image pixels
                     dst[x, y] = currentPixel;
--- README.md
+++ README.md
@@ -1,4 +1,4 @@
 # CLUTPlugin
 A work-in-progress color lookup table plugin for Paint.NET
 
-(Red works, green mostly works, blue is broken)
+Based on Eskil Steenberg's Clut work: http://www.quelsolaar.com/technology/clut.html
